dfc61323fb6263a04b15c3963d6bdd5b3b00b748
Ben Backhouse <bennylb0@gmail.com>
2016-06-03 21:51:09 +1000

    Remove bookmarks+ file

--- init.el
+++ init.el
@@ -12,7 +12,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(custom-enabled-themes (quote (material)))
  '(custom-safe-themes t)
  '(explicit-shell-file-name "/bin/zsh")
@@ -189,6 +188,7 @@
 	 ("<f2> u"	.	counsel-unicode-char)
 	 ("C-c g"	.	counsel-git)
 	 ("C-c j"	.	counsel-git-grep)
+	 ("C-M-n"^)
 	 )
   :config
   (progn
