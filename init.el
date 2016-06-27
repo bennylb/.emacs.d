d008290697bb73fe1ef3d5671f9c19b04e561828
Ben Backhouse <bennylb0@gmail.com>
2016-06-27 12:01:59 +1000

    Misc irony-mode modifications

--- init.el
+++ init.el
@@ -529,7 +529,8 @@
       (define-key irony-mode-map [remap complete-symbol]
 	'irony-completion-at-point-async))
     (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)))
+    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+  :ensure t)
 
 (use-package elpy
   :commands (elpy-mode)
